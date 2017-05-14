b4d6a4cb236fc60646ebbcb6f4e3ffa5f641287c
SarahSchaab <sarahschaab@me.com>
2017-05-13 22:32:19 -0400

    figured out the conditionals

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,6 +7,9 @@ age = gets.chomp.to_i
 puts "What year were you born?"
 year_born = gets.chomp.to_i
 
+if (2017-age) != year_born
+  end
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you? Yes or No"
 garlic = gets.chomp
 
@@ -21,6 +24,14 @@ insurance = gets.chomp
 unless  insurance == "yes"
 
 end
+
+if (2017-age) == year_born && garlic || insurance
+  puts "#{name} is probably not a vampire"
+elsif
+  (2017-age) != year_born && garlic || insurance
+    puts "#{name} is probably a vampire"
+end
+
 
 # if (2017 - current_age == year_born) = true &&
 #   (health_insurance == no) = false
