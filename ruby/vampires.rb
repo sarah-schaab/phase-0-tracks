2ef052489cd1bb348dbdfa7adbd2712da82150dd
SarahSchaab <sarahschaab@me.com>
2017-05-14 08:15:14 -0400

    make program loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,11 @@
+puts "How many Employees would you like to enter?"
+employee_num = gets.chomp.to_i
+
+employee_num.times do
+
 puts "What is your name?"
 name = gets.chomp
+
 
 puts "How old are you?"
 age = gets.chomp.to_i
@@ -14,7 +20,6 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you? Y
 garlic = gets.chomp
 
 unless  garlic == "yes"
-
 end
 
 
@@ -22,15 +27,43 @@ puts "Would you like to enroll in the company’s health insurance? Yes or No"
 insurance = gets.chomp
 
 unless  insurance == "yes"
-
 end
 
-if (2017-age) == year_born && garlic || insurance
+puts "Do you have any allergies? Yes or No"
+allergy = gets.chomp
+
+if allergy == "no"
+  puts "you don't have any allergies"
+else
+puts "Enter one Allergy at a time, and enter 'done' when finished listing allergies"
+
+allergic_to = gets.chomp
+until (allergic_to == "sunshine" or allergic_to == "done")
+    puts "Please enter your allergies."
+end
+end
+
+
+
+
+
+if (2017-age) == year_born && (garlic == true || insurance == true)
   puts "#{name} is probably not a vampire"
 elsif
-  (2017-age) != year_born && garlic || insurance
+  ((2017-age) != year_born) && (garlic == false || insurance == false)
     puts "#{name} is probably a vampire"
+elsif
+  ((2017-age) != year_born) && garlic == false && insurance == false
+    puts "#{name} is most certainly a vampire"
+elsif
+  name = "Drake Ula" || "Tu Fang"
+  puts "#{name} “Definitely a vampire."
+
+else
+  puts "Results inconclusive."
 end
+end
+
 
 
 # if (2017 - current_age == year_born) = true &&
