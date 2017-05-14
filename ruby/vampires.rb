afa470d027ad4bf8703298374ee7575c02a977c6
SarahSchaab <sarahschaab@me.com>
2017-05-13 22:24:59 -0400

    finish questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,7 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-current_age = gets.chomp.to_i
+age = gets.chomp.to_i
 
 puts "What year were you born?"
 year_born = gets.chomp.to_i
@@ -10,14 +10,23 @@ year_born = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you? Yes or No"
 garlic = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance? Yes or No"
-health_insurance = gets.chomp
+unless  garlic == "yes"
 
-if (2017 - current_age == year_born) = true &&
-  (health_insurance == no) = false
-  puts "#{name} is probably not a vampire"
-elsif
-  (2017 - current_age != year_born) &&
-  (health_insurance == false || garlic == false)
-  puts "#{name} is probably  a vampire"
 end
+
+
+puts "Would you like to enroll in the company’s health insurance? Yes or No"
+insurance = gets.chomp
+
+unless  insurance == "yes"
+
+end
+
+# if (2017 - current_age == year_born) = true &&
+#   (health_insurance == no) = false
+#   puts "#{name} is probably not a vampire"
+# elsif
+#   (2017 - current_age != year_born) &&
+#   (health_insurance == false || garlic == false)
+#   puts "#{name} is probably  a vampire"
+# end
