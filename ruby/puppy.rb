c8f89106c77ba39d2c838d1a5f4fd1db05658a97
SarahSchaab <sarahschaab@me.com>
2017-05-23 09:28:14 -0400

    add characteristics and behaviour to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin
+CLASS / SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Fur length: varies
+Friendly: yes
+Playful: yes
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+sleep
+eat
+play
+=end
