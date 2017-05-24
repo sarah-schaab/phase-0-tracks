eb07c91fad20c5f5a368c547a7a782e8383c2df2
SarahSchaab <sarahschaab@me.com>
2017-05-24 08:33:55 -0400

    release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,9 +3,9 @@ class Santa #this should be capitalized first letter
         @name = name
         @gender = gender
         @ethnicity = ethnicity
+        @age = 0
         @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
-      end
+    end
 
 
     def speak()
@@ -17,7 +17,56 @@ class Santa #this should be capitalized first letter
     end
 
     def about
-      p "name: #{@name}"
+      p "Name: #{@name}"
+      p "Gender: #{@gender}"
+      p "Ethnicity: #{@ethnicity} "
+      p "Reindeer Ranking: #{@reindeer_ranking}"
+      p "Age: #{@age} years old"
+    end
+
+    def celebrate_birthday(age)
+      p @age + 1
+      puts "Happy birthday #{@name}!!!"
+    end
+
+    def get_mad_at()
+      p reindeer_ranking.rotate(1)
+    end
+
+
+#____GETTER METHODS____#
+    def name
+      @name
+    end
+
+    def gender
+      @gender
+    end
+
+    def ethnicity
+      @ethnicity
+    end
+
+    def age
+      @age
+    end
+
+    def reindeer_ranking
+      @reindeer_ranking
+    end
+
+#____SETTER METHODS____#
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+    def gender=(new_gender)
+      @gender = new_gender
+    end
+
+    def age=(new_age)
+      @age = new_age
     end
 
 
@@ -27,8 +76,19 @@ end
 
 #____DRIVER CODE____#
 
-kris_kringle = Santa.new("Kris Kringle")
+kris_kringle = Santa.new("Kris Kringle", "Male", "Dominican")
+
 p kris_kringle
+
 p kris_kringle.speak()
+
 p kris_kringle.eat_milk_and_cookies("snicker doodle")
+
  kris_kringle.about
+
+ kris_kringle.gender = "female"
+
+ kris_kringle.celebrate_birthday(@age)
+ p kris_kringle.age
+
+ kris_kringle.get_mad_at
