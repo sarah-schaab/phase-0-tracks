72125bc0c388bab1e9c8193d791375715cc83de0
SarahSchaab <sarahschaab@me.com>
2017-05-25 08:13:17 -0400

    release 1 & 2

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,74 @@
+# module Shout
+
+#     def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#     def self.yelling_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
+
 module Shout
 
-    def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yelling_angrily(words)
+    words.upcase + "!!!" + ":("
   end
 
-    def self.yelling_happily(words)
-    words + "!!!" + " :D"
+  def yelling_happily(words)
+    words.upcase + "!!!" + ":D"
   end
+
 end
 
+class Dad_at_baseballgame
+  include Shout
+end
 
+class Mom_at_baseballgame
+  include Shout
+end
 
 
 #_______DRIVER CODE______#
 
-p Shout.yell_angrily("I'm angry!")
-p Shout.yelling_happily("I'm happy!")
+# p Shout.yell_angrily("I'm angry!")
+# p Shout.yelling_happily("I'm happy!")
+
+dad = Dad_at_baseballgame.new
+p dad.yelling_angrily("Don't drop the ball")
+
+mom = Mom_at_baseballgame.new
+p mom.yelling_happily("you got this")
+
+
+
+#_____SELF PRACTICE____#
+# module Actions
+#   def climb_up(object)
+#     p "IT'S CLIMBING UP THE #{object}"
+#   end
+
+
+#   def swim(object)
+#     p "IT'S SWIMMING IN THE #{object}"
+#   end
+# end
+
+# class Bear
+#   include Actions
+# end
+
+# class Rockclimber
+#   include Actions
+# end
+
+
+
+
+#___SELF PRACTICE___#
+# person = Rockclimber.new
+# person.swim("lake")
+# bear = Bear.new
+# bear.climb_up("house")
 
