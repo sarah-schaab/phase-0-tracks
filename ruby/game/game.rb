0bfcf0dbc24225c7318c0841b6f97c1b506a6797
SarahSchaab <sarahschaab@me.com>
2017-05-29 20:21:52 -0400

    solved 6.7git add .

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,4 +1,5 @@
-=begin !!!!!!PSEUDOCODE!!!!!!
+
+ # !!!!!!PSEUDOCODE!!!!!!
 # ###GAME####
 # # =>  One user can enter a word
 # # =>  Another user attempts to guess the word
@@ -23,7 +24,6 @@
 #     if array_of_word_letters includes letter (if array_of_word_letters.include?(letter))
 #     p "you guessed correctly"
 
-#     if
 #   if the guess limit is counted down to zero and word not exposed
 #   -put "you ran out of guesses
 #   -run game_over == true
@@ -32,26 +32,16 @@
 #     -run game_over == true
 
 
-# elsif
-# **** if guess_limit == guess_count
-#       p "you suck you lost"
-#       end
-=end
 #_____THE REAL STUFF!!!!!!_____#
-
-
-#____WORD GUESSING GAME____#
 #__CLASS__#
 class Game
-  attr_reader :secret_word, :guess_limit
-  attr_writer :secret_word, :guess_limit
+  # attr_reader :secret_word, :guess_limit
+  # attr_writer :secret_word, :guess_limit
   attr_accessor :secret_word, :guess_limit
-
 
   def initialize(secret_word)
     @secret_word = secret_word.downcase.split('')
     @number_of_guesses = 0
-    @guess_limit = guess_limit
     @previous_guesses = []
   end
 
@@ -59,19 +49,6 @@ class Game
     @secret_word = secret_word
   end
 
-  def guess_limit # must correspond to the word length
-    @guess_limit = secret_word.length * 2
-  end
-
-
-   def hide_word  #hiding word - tried GSUB, SUB, now replacing letters in word
-      hidden_word = @secret_word
-      replaced_letter = ""
-      @secret_word.length.times do |hidden_word|
-        replaced_letter += "_"
-      end
-      replaced_letter
-    end
   def previous_guesses
     @previous_guesses
   end
@@ -80,164 +57,73 @@ class Game
     @previous_guesses << letter
   end
 
-  def remove_from_guess_limit
-    @guess_limit = guess_limit - 1
+  def check_previous_guesses(letter)
+    @previous_guesses.include?(letter)
   end
 
-
-
+  def progress(secret_word, previous_guesses)
+    progress_string = ''
+    @secret_word.each do |letter|
+      if @previous_guesses.include?(letter)
+        progress_string << letter
+      else
+        progress_string << '_'
+      end
+    end
+    progress_string
+  end
 end
 
 #________USER INTERFACE________#
-guessed_letters = []
-solved_letters = []
-unguessed_letter = "_"
-hidden_word_array = []
-new_array = []
-
-
-# #initialize game with word entered
 puts "Player 1, enter a secret word for Player 2 to guess"
 secret_word = gets.chomp
+#initialize game with word entered
 game = Game.new(secret_word)
 game.guess_limit
 
 
-
-# hidden_word = secret_word.length
-# hidden_word.times {solved_letters << unguessed_letter}
-
-# trial = "example"
-# puts "Type a letter: "
-# guess = gets.chomp
-# puts trial.tr("^#{guess}", "*")
-
-hidden_word_array.map { |a| a - "*" }
-p hidden_word_array
-
-
-
 puts "Player 2, you have #{(secret_word.length + 4)} chances to win the game."
- guesses = secret_word.length + 4
+
+guesses = secret_word.length + 4
 while guesses > 0
   puts "Please guess a letter."
   guessed_letter = gets.chomp
-  game.add_to_previous_guesses(guessed_letter)
   guesses = guesses - 1
+  game.previous_guesses
 
-  if game.word.include?(guessed_letter)
+  if
+    game.previous_guesses.include?(guessed_letter) == true
+    puts "You already guessed #{guessed_letter}"
+    guesses += 1
+
+
+  elsif game.word.include?(guessed_letter)
+      game.add_to_previous_guesses(guessed_letter)
         puts "you guessed a letter correctly!"
         puts "you have #{guesses} chances left"
-        p secret_word.tr!("^#{guessed_letter}", "*")
-        #p hidden_word_array
-
-  elsif
-      game.word.include?(guessed_letter) == false
-      puts "This letter is NOT in the word!"
-      puts "you have #{guesses} chances left"
-
-
-  elsif game.previous_guesses.include?(guessed_letter)
-    puts "You already guessed that!. Try again."
-    guesses += 1
+         #secret_word.tr!("^#{guessed_letter}", "*")
+        #trying to figure out how to print the progress using the above information
+      p game.progress(secret_word, @previous_guesses)
+          if game.progress(secret_word, @previous_guesses) == secret_word
+          break
+        end
+  else
+    game.word.include?(guessed_letter) == false
+    game.add_to_previous_guesses(guessed_letter)
+    puts "This letter is NOT in the word!"
     puts "you have #{guesses} chances left"
-    p "Letters you already guessed #{guessed_letters}"
-end
-#new_array << hidden_word_array.map! { |a| a.delete "*" }
-#p hidden_word_array
-
-
-
-
-
-
-
-
-
+    p game.progress(secret_word, @previous_guesses)
+  end
 
 end
 
+if game.progress(secret_word, @previous_guesses) == secret_word
+  puts "YOU WON!"
+elsif
+  game.progress(secret_word, @previous_guesses) != secret_word
+  puts "YOU LOST THE GAME!"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin____THIS IS ALL PRACTICE STUFF_____
-
-# #if letter is in array_of_letters
-#   #show the correct letters guessed
-#   #puts "Player 2, you have #{game.guess_limit - 1} guesses left."
-# #elsif letter is not in array_of_letters
-#   #puts "Player 2, you guessed incorrectly. You have #{game.guess_limit - 1} guesses left."
-#   #display current progress with correct letters
-# #elsif guess_limit == 0
-#  #@game_over = true
-#   #puts "You suck, you lost"
-# #elsif all letters are correct
-#   #@game_over = true
-#     #puts "YOU ARE SMART!"
-
-#!!!!!!______ATTEMPT 1______!!!!!!#
-# if word.include?(letter)
-#     puts "This letter is in the word!"
-#    @guess_limit = (game.guess_limit - 1)
-#     @guess_limit
-
-#   elsif
-#     word.include?(letter) == false
-#     puts "That letter is not in the word!"
-#     @guess_limit = (game.guess_limit - 1)
-#     @guess_limit
-#   elsif
-#     word.include?(letter) == false && previous_guesses.include?(letter)
-#     puts "You already guessed that letter."
-#     @guess_limit = (game.guess_limit)
-#     @guess_limit
-#   end
-
-
-
-
-# p game
-
-
-
-
-
-
-
-
+end
 
 
 
@@ -258,13 +144,8 @@ end
 # # game.hide_word
 
 
-# # game.guess_limit
-# # p game
-# # p game.guess_limit
 
 
-
-# =begin
 # while game_over == false
 
 #   if
@@ -294,5 +175,3 @@ end
 
 
 
-
-# =end
