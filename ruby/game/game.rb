4d061972f89b6255906316fee56420c643db0024
SarahSchaab <sarahschaab@me.com>
2017-05-31 16:13:05 -0400

    commit on game

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -47,6 +47,7 @@ class Game
 
   def word
     @secret_word = secret_word
+    p @secret_word
   end
 
   def previous_guesses
@@ -74,103 +75,103 @@ class Game
   end
 end
 
-#________USER INTERFACE________#
-puts "Player 1, enter a secret word for Player 2 to guess"
-secret_word = gets.chomp
-#initialize game with word entered
-game = Game.new(secret_word)
-game.guess_limit
+# #________USER INTERFACE________#
+# puts "Player 1, enter a secret word for Player 2 to guess"
+# secret_word = gets.chomp
+# #initialize game with word entered
+# game = Game.new(secret_word)
+# game.guess_limit
 
 
-puts "Player 2, you have #{(secret_word.length + 4)} chances to win the game."
+# puts "Player 2, you have #{(secret_word.length + 4)} chances to win the game."
 
-guesses = secret_word.length + 4
-while guesses > 0
-  puts "Please guess a letter."
-  guessed_letter = gets.chomp
-  guesses = guesses - 1
-  game.previous_guesses
-
-  if
-    game.previous_guesses.include?(guessed_letter) == true
-    puts "You already guessed #{guessed_letter}"
-    guesses += 1
-
-
-  elsif game.word.include?(guessed_letter)
-      game.add_to_previous_guesses(guessed_letter)
-        puts "you guessed a letter correctly!"
-        puts "you have #{guesses} chances left"
-         #secret_word.tr!("^#{guessed_letter}", "*")
-        #trying to figure out how to print the progress using the above information
-      p game.progress(secret_word, @previous_guesses)
-          if game.progress(secret_word, @previous_guesses) == secret_word
-          break
-        end
-  else
-    game.word.include?(guessed_letter) == false
-    game.add_to_previous_guesses(guessed_letter)
-    puts "This letter is NOT in the word!"
-    puts "you have #{guesses} chances left"
-    p game.progress(secret_word, @previous_guesses)
-  end
-
-end
-
-if game.progress(secret_word, @previous_guesses) == secret_word
-  puts "YOU WON!"
-elsif
-  game.progress(secret_word, @previous_guesses) != secret_word
-  puts "YOU LOST THE GAME!"
-
-end
-
-
-
-# #___DRIVER CODE___#
-# # guessed_letters = []
-
-
-
-# # #Instructions for game
-# # p "Please enter a word for player 2 to guess"
-# # word = "filler"
-
-# # #initialize game with word entered
-# # game = WordGame.new(word)
-
-
-# # p "Player 2, you have #{game.guess_limit} guesses to go. "
-# # game.hide_word
-
-
-
-
-# while game_over == false
+# guesses = secret_word.length + 4
+# while guesses > 0
+#   puts "Please guess a letter."
+#   guessed_letter = gets.chomp
+#   guesses = guesses - 1
+#   game.previous_guesses
 
 #   if
-#     puts "guess a letter" (must be lowercase so .downcase)
-#     letter = gets.chomp
-#     if array of guesses includes letter (if array_of_guesses.include(letter))
-#     p "you already guessed #{letter}" (does not subtract from guess limit)
-#     if array of guesses does not include letter (if array_of_guesses.include?(letter))
-#     P "you guessed incorrectly"
-#     if array_of_word_letters includes letter (if array_of_word_letters.include?(letter))
-#     p "you guessed correctly"
-
-#     if
-#   if the guess limit is counted down to zero and word not exposed
-#   -put "you ran out of guesses
-#   -run game_over == true
-
-#   if the guess limit is counted down to zero and the word is exposed
-#     -run game_over == true
+#     game.previous_guesses.include?(guessed_letter) == true
+#     puts "You already guessed #{guessed_letter}"
+#     guesses += 1
 
 
+#   elsif game.word.include?(guessed_letter)
+#       game.add_to_previous_guesses(guessed_letter)
+#         puts "you guessed a letter correctly!"
+#         puts "you have #{guesses} chances left"
+#          #secret_word.tr!("^#{guessed_letter}", "*")
+#         #trying to figure out how to print the progress using the above information
+#       p game.progress(secret_word, @previous_guesses)
+#           if game.progress(secret_word, @previous_guesses) == secret_word
+#           break
+#         end
+#   else
+#     game.word.include?(guessed_letter) == false
+#     game.add_to_previous_guesses(guessed_letter)
+#     puts "This letter is NOT in the word!"
+#     puts "you have #{guesses} chances left"
+#     p game.progress(secret_word, @previous_guesses)
+#   end
+
+# end
+
+# if game.progress(secret_word, @previous_guesses) == secret_word
+#   puts "YOU WON!"
 # elsif
-# **** if guess_limit == guess_count
-#       p "you suck you lost"
-#       end
+#   game.progress(secret_word, @previous_guesses) != secret_word
+#   puts "YOU LOST THE GAME!"
+
+# end
+
+
+
+# # #___DRIVER CODE___#
+# # # guessed_letters = []
+
+
+
+# # # #Instructions for game
+# # # p "Please enter a word for player 2 to guess"
+# # # word = "filler"
+
+# # # #initialize game with word entered
+# # # game = WordGame.new(word)
+
+
+# # # p "Player 2, you have #{game.guess_limit} guesses to go. "
+# # # game.hide_word
+
+
+
+
+# # while game_over == false
+
+# #   if
+# #     puts "guess a letter" (must be lowercase so .downcase)
+# #     letter = gets.chomp
+# #     if array of guesses includes letter (if array_of_guesses.include(letter))
+# #     p "you already guessed #{letter}" (does not subtract from guess limit)
+# #     if array of guesses does not include letter (if array_of_guesses.include?(letter))
+# #     P "you guessed incorrectly"
+# #     if array_of_word_letters includes letter (if array_of_word_letters.include?(letter))
+# #     p "you guessed correctly"
+
+# #     if
+# #   if the guess limit is counted down to zero and word not exposed
+# #   -put "you ran out of guesses
+# #   -run game_over == true
+
+# #   if the guess limit is counted down to zero and the word is exposed
+# #     -run game_over == true
+
+
+# # elsif
+# # **** if guess_limit == guess_count
+# #       p "you suck you lost"
+# #       end
 
 
 
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,10 +1,14 @@
 
+require_relative 'game'
+describe Game do
 
+let(:game) { Game.new("hello")}
 
-it "changes letters in word to dashes" do
-expect(game.hide_word).to eq "_______"
+it "prints the word" do
+expect(game.word).to eq "hello"
   end
 
   it "changes number of guesses allowed to 2x the length of word entered" do
     expect(game.guess_limit).to eq 2 * word.length
   end
+end
