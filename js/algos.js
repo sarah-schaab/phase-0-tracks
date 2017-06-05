6604ce6655474ed6f7dc4a270768bc7a28c87f32
SarahSchaab <sarahschaab@me.com>
2017-06-05 07:50:20 -0400

    add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,18 @@
+/* Write a function that takes an array of multiple phrases or words and returns the longest word or phrase.
+["long phrase","longest phrase","longer phrase"], => "longest phrase"
+*/
+
+
+/* Write a function that takes two objects and checks to see if the objects share at least one key-value pair
+yourFunction({name: "Steven", age: 54}, {name: "Tamir", age: 54}); =>true
+yourFunction({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}); =>true
+*/
+
+/* Write a function that takes an integer for length, and builds and returns an array of strings of the given length. So if we ran your function with an argument of 3, we would get an array of 3 random words back.
+The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)*/
+
+
+
+
+
+/*Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.*/
